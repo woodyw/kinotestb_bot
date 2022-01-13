62303dd323beeb6e374725841f8e7d8dd9721712
woodyw <vadimgogolin@gmail.com>
2022-01-13 14:05:10 +0500

    11

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const TOKEN = '5067587687:AAEcajp02noUeTcdssWi7dLJvbIjSEuC_uk'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})
+
 bot.on('message', msg => {
 
     bot.sendMessage(msg.chat.id, 'Hello from TE, bot sase "Hi, ${msg.from.first_name}"')
